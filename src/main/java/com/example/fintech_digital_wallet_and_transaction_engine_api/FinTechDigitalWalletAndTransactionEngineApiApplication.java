a3098e99d82344d0498e64311891904792c0e02a
Tudor Gradinaru <tudor.rodut@yahoo.com>
2026-08-19 12:14:31 +0300

    Initial commit

---
+++ src/main/java/com/example/fintech_digital_wallet_and_transaction_engine_api/FinTechDigitalWalletAndTransactionEngineApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fintech_digital_wallet_and_transaction_engine_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTechDigitalWalletAndTransactionEngineApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinTechDigitalWalletAndTransactionEngineApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fintech_digital_wallet_and_transaction_engine_api/FinTechDigitalWalletAndTransactionEngineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fintech_digital_wallet_and_transaction_engine_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTechDigitalWalletAndTransactionEngineApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
